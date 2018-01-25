1a3dad94053cfc113c214bb5d44ed84b702d85fe
galihpw <galih1995@gmail.com>
2018-01-25 20:35:17 +0700

    add latihan

--- app/src/main/java/com/galihpw/smartbookfisika/Pager/Latihan.java
+++ app/src/main/java/com/galihpw/smartbookfisika/Pager/Latihan.java
@@ -5,14 +5,32 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
+import com.bumptech.glide.Glide;
 import com.galihpw.smartbookfisika.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by GalihPW on 25/01/2018.
  */
 
 public class Latihan extends Fragment {
+
+    @BindView(R.id.latihanPGImage)
+    ImageView imageLatihanPG;
+
+    @BindView(R.id.latihanPG)
+    RelativeLayout latihanPG;
+
+    @BindView(R.id.latihanFotoImage)
+    ImageView imageLatihanFoto;
+
+    @BindView(R.id.latihanFoto)
+    RelativeLayout latihanFoto;
 
     public View v;
 
@@ -26,6 +44,28 @@ public class Latihan extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.latihan, container, false);
 
+        ButterKnife.bind(this, v);
+
+        Glide.with(getActivity()).load(R.drawable.latihan_soal).into(imageLatihanPG);
+        Glide.with(getActivity()).load(R.drawable.latihan_soal).into(imageLatihanFoto);
+
+        /*latihanSoal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this,LatihanPGActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+            }
+        });*/
+
+       /* materiListrikStatis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent intent = new Intent(getActivity(),SubMenuMateri.class);
+                //startActivity(intent);
+                //overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+            }
+        });*/
 
         return v;
         //Returning the layout file after inflating
