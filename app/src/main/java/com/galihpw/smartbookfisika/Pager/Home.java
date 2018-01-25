5e2b056528413af64db588b8452657925e3721f7
sutrisnajie <sutrisnajie@gmail.com>
2018-01-25 15:22:39 +0700

    versi1

--- app/src/main/java/com/galihpw/smartbookfisika/Pager/Home.java
+++ app/src/main/java/com/galihpw/smartbookfisika/Pager/Home.java
@@ -1,32 +1,38 @@
 package com.galihpw.smartbookfisika.Pager;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.galihpw.smartbookfisika.Adapter.GridAdapter;
 import com.galihpw.smartbookfisika.R;
-import com.galihpw.smartbookfisika.slider.FragmentSlider;
-import com.galihpw.smartbookfisika.slider.SliderIndicator;
-import com.galihpw.smartbookfisika.slider.SliderPagerAdapter;
-import com.galihpw.smartbookfisika.slider.SliderView;
+import com.galihpw.smartbookfisika.Adapter.ViewPagerAdapter;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by GalihPW on 25/01/2018.
  */
 
-public class Home extends Fragment {
-
+public class Home extends Fragment implements View.OnClickListener {
+    ViewPager viewPager;
+    LinearLayout sliderDotspanel;
+    private int dotscount;
+    private ImageView[] dots;
     public View v;
-
-    private SliderView sliderView;
     private LinearLayout mLinearLayout;
+    RecyclerView mRecyclerView;
+    RecyclerView.LayoutManager mLayoutManager;
+    RecyclerView.Adapter mAdapter;
 
     @Override
     public void onResume(){
@@ -37,34 +43,104 @@ public class Home extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.home, container, false);
+        viewPager = (ViewPager) v.findViewById(R.id.viewPager);
 
-        sliderView = v.findViewById(R.id.sliderView);
-        mLinearLayout = v.findViewById(R.id.pagesContainer);
-        setupSlider();
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager  = new GridLayoutManager(getActivity(),2);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new GridAdapter();
+        mRecyclerView.setAdapter(mAdapter);
 
-        //Intent inte = new Intent(getActivity(), BuatBiodata.class);
-        //getActivity().startActivity(inte);
+        sliderDotspanel = (LinearLayout) v.findViewById(R.id.SliderDots);
 
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(v.getContext());
+
+        viewPager.setAdapter(viewPagerAdapter);
+
+        dotscount = viewPagerAdapter.getCount();
+        dots = new ImageView[dotscount];
+
+        for(int i = 0; i < dotscount; i++){
+
+            dots[i] = new ImageView(getActivity());
+            dots[i].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.nonactive_dot));
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+            params.setMargins(8, 0, 8, 0);
+
+            sliderDotspanel.addView(dots[i], params);
+
+        }
+
+        dots[0].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.active_dot));
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+                for(int i = 0; i< dotscount; i++){
+                    dots[i].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.nonactive_dot));
+                }
+
+                dots[position].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.active_dot));
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new MyTimerTask(), 2000, 4000);
         return v;
-        //Returning the layout file after inflating
-        //Change R.layout.tab1 in you classes
-//        return inflater.inflate(R.layout.Home, container, false);
     }
 
-    private void setupSlider() {
-        sliderView.setDurationScroll(800);
-        List<Fragment> fragments = new ArrayList<>();
-        fragments.add(FragmentSlider.newInstance("https://image.tmdb.org/t/p/w250_and_h141_bestv2/zYFQM9G5j9cRsMNMuZAX64nmUMf.jpg"));
-        fragments.add(FragmentSlider.newInstance("https://image.tmdb.org/t/p/w250_and_h141_bestv2/rXBB8F6XpHAwci2dihBCcixIHrK.jpg"));
-        fragments.add(FragmentSlider.newInstance("https://image.tmdb.org/t/p/w250_and_h141_bestv2/biN2sqExViEh8IYSJrXlNKjpjxx.jpg"));
-        fragments.add(FragmentSlider.newInstance("https://image.tmdb.org/t/p/w250_and_h141_bestv2/o9OKe3M06QMLOzTl3l6GStYtnE9.jpg"));
+    @Override
+    public void onClick(View view) {
 
-        SliderPagerAdapter mAdapter = new SliderPagerAdapter(getActivity().getSupportFragmentManager(), fragments);
-        sliderView.setAdapter(mAdapter);
-        SliderIndicator mIndicator = new SliderIndicator(getActivity(), mLinearLayout, sliderView, R.drawable.indicator_circle);
-        mIndicator.setPageCount(fragments.size());
-        mIndicator.show();
     }
 
+    public class MyTimerTask extends TimerTask {
 
+        @Override
+        public void run() {
+
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+
+                    if(viewPager.getCurrentItem() == 0){
+                        viewPager.setCurrentItem(1);
+                    } else if(viewPager.getCurrentItem() == 1){
+                        viewPager.setCurrentItem(2);
+                    } else if(viewPager.getCurrentItem()==2){
+                        viewPager.setCurrentItem(3);
+                    }
+                    else if(viewPager.getCurrentItem()==3){
+                        viewPager.setCurrentItem(4);
+                    }
+                    else if(viewPager.getCurrentItem()==4){
+                        viewPager.setCurrentItem(5);
+                    }
+                    else if(viewPager.getCurrentItem()==5){
+                        viewPager.setCurrentItem(6);
+                    }
+                    else if(viewPager.getCurrentItem()==6){
+                        viewPager.setCurrentItem(0);
+                    }
+
+
+                }
+            });
+        }
+    }
 }
