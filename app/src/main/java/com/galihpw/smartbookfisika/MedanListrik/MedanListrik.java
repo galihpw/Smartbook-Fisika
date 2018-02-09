366d0c248050a0b597205889f3484730f452a995
sutrisnajie <sutrisnajie@gmail.com>
2018-02-09 13:27:09 +0700

    materi370%

--- app/src/main/java/com/galihpw/smartbookfisika/MedanListrik/MedanListrik.java
+++ app/src/main/java/com/galihpw/smartbookfisika/MedanListrik/MedanListrik.java
@@ -20,12 +20,31 @@ public class MedanListrik extends AppCompatActivity {
         setContentView(R.layout.activity_medan_listrik);
         TextView teksmd7 = (TextView)findViewById(R.id.teksmd7);
         TextView teksmd9 = (TextView)findViewById(R.id.teksmd9);
+        TextView teksmd36 = (TextView)findViewById(R.id.teksmd36);
+        TextView teksmd38 = (TextView)findViewById(R.id.teksmd38);
+        TextView teksmd45 = (TextView)findViewById(R.id.teksmd45);
+        TextView teksmd50 = (TextView)findViewById(R.id.teksmd50);
+        TextView teksmd56 = (TextView)findViewById(R.id.teksmd56);
+        TextView teksmd58 = (TextView)findViewById(R.id.teksmd58);
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             teksmd7.setText(Html.fromHtml(getString(R.string.teks7md), Html.FROM_HTML_MODE_COMPACT));
             teksmd9.setText(Html.fromHtml(getString(R.string.teks9md), Html.FROM_HTML_MODE_COMPACT));
+            teksmd36.setText(Html.fromHtml(getString(R.string.teks36md), Html.FROM_HTML_MODE_COMPACT));
+            teksmd38.setText(Html.fromHtml(getString(R.string.teks38md), Html.FROM_HTML_MODE_COMPACT));
+            teksmd45.setText(Html.fromHtml(getString(R.string.teks45md), Html.FROM_HTML_MODE_COMPACT));
+            teksmd50.setText(Html.fromHtml(getString(R.string.teks50md), Html.FROM_HTML_MODE_COMPACT));
+            teksmd56.setText(Html.fromHtml(getString(R.string.teks56md), Html.FROM_HTML_MODE_COMPACT));
+            teksmd58.setText(Html.fromHtml(getString(R.string.teks58md), Html.FROM_HTML_MODE_COMPACT));
         }else{
             teksmd7.setText(Html.fromHtml(getString(R.string.teks7md)));
             teksmd9.setText(Html.fromHtml(getString(R.string.teks9md)));
+            teksmd36.setText(Html.fromHtml(getString(R.string.teks36md)));
+            teksmd38.setText(Html.fromHtml(getString(R.string.teks38md)));
+            teksmd45.setText(Html.fromHtml(getString(R.string.teks45md)));
+            teksmd50.setText(Html.fromHtml(getString(R.string.teks50md)));
+            teksmd56.setText(Html.fromHtml(getString(R.string.teks56md)));
+            teksmd58.setText(Html.fromHtml(getString(R.string.teks58md)));
         }
         final GifImageView gif = (GifImageView)findViewById(R.id.mdgif1);
         gif.setImageResource(R.drawable.img1);
