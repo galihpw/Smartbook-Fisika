55d20f9cfce0c4185b797af4875fa67b6f60bb68
galihpw <galih1995@gmail.com>
2018-02-13 10:53:45 +0700

    materi 2 dan 5

--- app/src/main/java/com/galihpw/smartbookfisika/LatihanPG/TabSoal1.java
+++ app/src/main/java/com/galihpw/smartbookfisika/LatihanPG/TabSoal1.java
@@ -2,6 +2,7 @@ package com.galihpw.smartbookfisika.LatihanPG;
 
 import android.os.Bundle;
 import android.support.annotation.IdRes;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,15 +10,22 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.galihpw.smartbookfisika.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by GalihPW on 24/04/2017.
  */
 
 public class TabSoal1 extends Fragment {
+
+    @BindView(R.id.hintButton)
+    FloatingActionButton hintButton;
 
     private RadioGroup rG;
     private RadioButton rB;
@@ -28,6 +36,8 @@ public class TabSoal1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.tab_soal1, container, false);
+
+        ButterKnife.bind(this,rootView);
 
         bSelesai = (Button) getActivity().findViewById(R.id.bSelesai);
 
@@ -42,11 +52,19 @@ public class TabSoal1 extends Fragment {
                 // mencari radio button
                 rB = (RadioButton) rootView.findViewById(selectedId);
 
-                if(rB.getText().equals("a. ya")){
+                if(rB.getText().equals("Batang kaca berkurang elektronnya, karena pada saat penggosokan dengan bulu, elektron dari batang kaca berpindah ke bulu.")){
                     Toast.makeText(getActivity(), "Benar", Toast.LENGTH_SHORT).show();
                 }else{
                     Toast.makeText(getActivity(), "Salah", Toast.LENGTH_SHORT).show();
+                    hintButton.setVisibility(View.VISIBLE);
                 }
+            }
+        });
+
+        hintButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                hintButton.setVisibility(View.INVISIBLE);
             }
         });
 
