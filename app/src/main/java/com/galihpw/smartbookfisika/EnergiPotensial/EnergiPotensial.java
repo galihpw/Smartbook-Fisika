26c8e9179d647a6c06b8905cb710ece8057110b2
sutrisnajie <sutrisnajie@gmail.com>
2018-02-15 06:51:04 +0700

    bab4

--- app/src/main/java/com/galihpw/smartbookfisika/EnergiPotensial/EnergiPotensial.java
+++ app/src/main/java/com/galihpw/smartbookfisika/EnergiPotensial/EnergiPotensial.java
@@ -22,23 +22,77 @@ public class EnergiPotensial extends AppCompatActivity {
     ImageView image7ep;
     @BindView(R.id.image8ep)
     ImageView image8ep;
+    @BindView(R.id.image12ep)
+    ImageView image12ep;
+    @BindView(R.id.image13ep)
+    ImageView image13ep;
+    @BindView(R.id.image15ep)
+    ImageView image15ep;
+    @BindView(R.id.image21ep)
+    ImageView image21ep;
+    @BindView(R.id.image24ep)
+    ImageView image24ep;
+    @BindView(R.id.image27ep)
+    ImageView image27ep;
+    @BindView(R.id.image31ep)
+    ImageView image31ep;
+    @BindView(R.id.image35ep)
+    ImageView image35ep;
+    @BindView(R.id.image38ep)
+    ImageView image38ep;
+    @BindView(R.id.image40ep)
+    ImageView image40ep;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_energi_potensial);
         TextView teksep6 = (TextView)findViewById(R.id.teksep6);
         TextView teksep9 = (TextView)findViewById(R.id.teksep9);
+        TextView teksep16 = (TextView)findViewById(R.id.teksep16);
+        TextView teksep17 = (TextView)findViewById(R.id.teksep17);
+        TextView teksep21 = (TextView)findViewById(R.id.teksep21);
+        TextView teksep24 = (TextView)findViewById(R.id.teksep24);
+        TextView teksep28 = (TextView)findViewById(R.id.teksep28);
+        TextView teksep39 = (TextView)findViewById(R.id.teksep39);
+        TextView teksep44 = (TextView)findViewById(R.id.teksep44);
+
+
         ButterKnife.bind(this);
         Glide.with(this).load(R.drawable.image1ep).into(image1ep);
         Glide.with(this).load(R.drawable.image2ep).into(image2ep);
         Glide.with(this).load(R.drawable.image7ep).into(image7ep);
         Glide.with(this).load(R.drawable.image8ep).into(image8ep);
+        Glide.with(this).load(R.drawable.imageep12).into(image12ep);
+        Glide.with(this).load(R.drawable.imageep13).into(image13ep);
+        Glide.with(this).load(R.drawable.imageep15).into(image15ep);
+        Glide.with(this).load(R.drawable.imageep21).into(image21ep);
+        Glide.with(this).load(R.drawable.imageep242).into(image24ep);
+        Glide.with(this).load(R.drawable.imageep27).into(image27ep);
+        Glide.with(this).load(R.drawable.imageep31).into(image31ep);
+        Glide.with(this).load(R.drawable.imageep35).into(image35ep);
+        Glide.with(this).load(R.drawable.imageep38).into(image38ep);
+        Glide.with(this).load(R.drawable.imageep40).into(image40ep);
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             teksep6.setText(Html.fromHtml(getString(R.string.teksep6), Html.FROM_HTML_MODE_COMPACT));
             teksep9.setText(Html.fromHtml(getString(R.string.teksep9), Html.FROM_HTML_MODE_COMPACT));
+            teksep16.setText(Html.fromHtml(getString(R.string.teksep16), Html.FROM_HTML_MODE_COMPACT));
+            teksep17.setText(Html.fromHtml(getString(R.string.teksep17), Html.FROM_HTML_MODE_COMPACT));
+            teksep21.setText(Html.fromHtml(getString(R.string.teksep21), Html.FROM_HTML_MODE_COMPACT));
+            teksep24.setText(Html.fromHtml(getString(R.string.teksep24), Html.FROM_HTML_MODE_COMPACT));
+            teksep28.setText(Html.fromHtml(getString(R.string.teksep28), Html.FROM_HTML_MODE_COMPACT));
+            teksep39.setText(Html.fromHtml(getString(R.string.teksep39), Html.FROM_HTML_MODE_COMPACT));
+            teksep44.setText(Html.fromHtml(getString(R.string.teksep44), Html.FROM_HTML_MODE_COMPACT));
         }else {
             teksep6.setText(Html.fromHtml(getString(R.string.teksep6)));
             teksep9.setText(Html.fromHtml(getString(R.string.teksep9)));
+            teksep16.setText(Html.fromHtml(getString(R.string.teksep16)));
+            teksep17.setText(Html.fromHtml(getString(R.string.teksep17)));
+            teksep21.setText(Html.fromHtml(getString(R.string.teksep21)));
+            teksep24.setText(Html.fromHtml(getString(R.string.teksep24)));
+            teksep28.setText(Html.fromHtml(getString(R.string.teksep28)));
+            teksep39.setText(Html.fromHtml(getString(R.string.teksep39)));
+            teksep44.setText(Html.fromHtml(getString(R.string.teksep44)));
         }
     }
 }
